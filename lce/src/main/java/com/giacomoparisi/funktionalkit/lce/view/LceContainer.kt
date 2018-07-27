05db7d7fd046961715e32c335d55c4ba64d8e4a8
Giacomo Parisi <giacomoparisi95@gmail.com>
2018-07-28 01:32:47 +0200

    Remove lce settings and add builder

--- lce/src/main/java/com/giacomoparisi/funktionalkit/lce/view/LceContainer.kt
+++ lce/src/main/java/com/giacomoparisi/funktionalkit/lce/view/LceContainer.kt
@@ -6,6 +6,8 @@ import android.view.View
 import android.view.ViewGroup
 import android.view.ViewGroup.LayoutParams.MATCH_PARENT
 import android.widget.TextView
+import androidx.annotation.IdRes
+import androidx.annotation.LayoutRes
 import androidx.coordinatorlayout.widget.CoordinatorLayout
 import androidx.core.view.children
 import arrow.core.*
@@ -14,9 +16,16 @@ import com.giacomoparisi.funktionalkit.extensions.visibleOrGone
 import com.giacomoparisi.funktionalkit.lce.Lce
 import com.giacomoparisi.funktionalkit.lce.R
 
-class LceContainer<T>(
+class LceContainer<T> private constructor(
         context: Context,
-        private val _lceSettings: LceContainerSettings
+        private var _loadingLayoutId: Option<Int> = none(),
+        private var _errorLayoutId: Option<Int> = none(),
+        private var _errorMessageId: Option<Int> = none(),
+        private var _errorRetryId: Option<Int> = none(),
+        private var _retryAction: Option<() -> Unit> = none(),
+        private var _networkErrorMessageRes: Option<Int> = none(),
+        private var _onError: Option<(error: String, view: View, retry: Option<() -> Unit>) -> Unit> = none(),
+        private var _onLoading: Option<() -> Unit> = none()
 ) : CoordinatorLayout(context) {
 
     private var _loading: Option<View> = none()
@@ -30,7 +39,7 @@ class LceContainer<T>(
                 is Lce.Loading -> {
                     if (this.showLoading) {
                         this.children.forEach { it.visibleOrGone = it == this._loading.orNull() || it != this._error.orNull() }
-                        this._lceSettings.onLoading.map { it.invoke() }
+                        this._onLoading.map { it.invoke() }
                     } else
                         this.children.forEach { it.visibleOrGone = it != this._loading.orNull() && it != this._error.orNull() }
                 }
@@ -41,12 +50,12 @@ class LceContainer<T>(
                     if (this.showError) {
                         this.children.forEach { it.visibleOrGone = it == this._error.orNull() || it != this._loading.orNull() }
                         if (value.connectionError) {
-                            val networkError = context.getString(_lceSettings.networkErrorMessageRes.getOrElse { R.string.ERROR_Network })
+                            val networkError = context.getString(_networkErrorMessageRes.getOrElse { R.string.ERROR_Network })
                             this._errorMessage.map { it.text = networkError }
-                            this._lceSettings.onError.map { it.invoke(networkError, this, this._lceSettings.retryAction) }
+                            this._onError.map { it.invoke(networkError, this, this._retryAction) }
                         } else {
                             this._errorMessage.map { it.text = value.message }
-                            this._lceSettings.onError.map { it.invoke(value.message, this, this._lceSettings.retryAction) }
+                            this._onError.map { it.invoke(value.message, this, this._retryAction) }
                         }
                     }
                 }
@@ -61,15 +70,72 @@ class LceContainer<T>(
 
     var showError: Boolean = true
 
+    data class Builder<T>(val context: Context) {
+
+        private var _loadingLayoutId: Option<Int> = none()
+        private var _errorLayoutId: Option<Int> = none()
+        private var _errorMessageId: Option<Int> = none()
+        private var _errorRetryId: Option<Int> = none()
+        private var _retryAction: Option<() -> Unit> = none()
+        private var _networkErrorMessageRes: Option<Int> = none()
+        private var _onError: Option<(error: String, view: View, retry: Option<() -> Unit>) -> Unit> = none()
+        private var _onLoading: Option<() -> Unit> = none()
+
+        fun Builder<T>.error(
+                @LayoutRes layoutId: Int,
+                @IdRes errorMessageId: Int?,
+                @IdRes errorRetryId: Int?,
+                retryAction: () -> Unit,
+                networkErrorMessageRes: Int?,
+                onError: ((error: String, view: View, retry: Option<() -> Unit>) -> Unit)?
+        ): Builder<T> {
+            this._errorLayoutId = layoutId.toOption()
+            this._errorMessageId = errorMessageId.toOption()
+            this._errorRetryId = errorRetryId.toOption()
+            this._retryAction = retryAction.toOption()
+            this._networkErrorMessageRes = networkErrorMessageRes.toOption()
+            this._onError = onError.toOption()
+            return this@Builder
+        }
+
+        fun Builder<T>.error(
+                networkErrorMessageRes: Int?,
+                onError: ((error: String, view: View, retry: Option<() -> Unit>) -> Unit)
+        ): Builder<T> {
+            this._networkErrorMessageRes = networkErrorMessageRes.toOption()
+            this._onError = onError.toOption()
+            return this@Builder
+        }
+
+        fun Builder<T>.loading(@LayoutRes layoutId: Int, onLoading: (() -> Unit)?): Builder<T> {
+            this._loadingLayoutId = layoutId.toOption()
+            this._onLoading = onLoading.toOption()
+            return this@Builder
+        }
+
+        fun build(): LceContainer<T> =
+                LceContainer(
+                        this@Builder.context,
+                        this@Builder._loadingLayoutId,
+                        this@Builder._errorLayoutId,
+                        this@Builder._errorMessageId,
+                        this@Builder._errorRetryId,
+                        this@Builder._retryAction,
+                        this@Builder._networkErrorMessageRes,
+                        this@Builder._onError,
+                        this@Builder._onLoading
+                )
+    }
+
     fun attachTo(view: View) {
         if (!isInEditMode) {
             this.addView(view)
             val inflater = LayoutInflater.from(context)
-            _loading = _lceSettings.loadingLayoutId.map { inflater.inflate(it, this, false).also { addView(it) } }
-            _error = _lceSettings.errorLayoutId.map { inflater.inflate(it, this, false).also { addView(it) } }
-            _errorMessage = Option.monad().binding { _error.bind().findViewById<TextView>(_lceSettings.errorMessageId.bind()) }.fix()
-            _retry = Option.monad().binding { _error.bind().findViewById<View>(_lceSettings.errorRetryId.bind()) }.fix()
-            _retry.map { it.setOnClickListener { _lceSettings.retryAction.map { it() } } }
+            _loading = _loadingLayoutId.map { inflater.inflate(it, this, false).also { addView(it) } }
+            _error = _errorLayoutId.map { inflater.inflate(it, this, false).also { addView(it) } }
+            _errorMessage = Option.monad().binding { _error.bind().findViewById<TextView>(_errorMessageId.bind()) }.fix()
+            _retry = Option.monad().binding { _error.bind().findViewById<View>(_errorRetryId.bind()) }.fix()
+            _retry.map { it.setOnClickListener { _retryAction.map { it() } } }
 
             val parent = (view.parent as ViewGroup?).toOption()
 
@@ -86,11 +152,11 @@ class LceContainer<T>(
             val params = android.view.ViewGroup.LayoutParams(MATCH_PARENT, MATCH_PARENT)
             this.addView(view, params)
             val inflater = LayoutInflater.from(context)
-            _loading = _lceSettings.loadingLayoutId.map { inflater.inflate(it, this, false).also { addView(it, params) } }
-            _error = _lceSettings.errorLayoutId.map { inflater.inflate(it, this, false).also { addView(it, params) } }
-            _errorMessage = Option.monad().binding { _error.bind().findViewById<TextView>(_lceSettings.errorMessageId.bind()) }.fix()
-            _retry = Option.monad().binding { _error.bind().findViewById<View>(_lceSettings.errorRetryId.bind()) }.fix()
-            _retry.map { it.setOnClickListener { _lceSettings.retryAction.map { it() } } }
+            _loading = _loadingLayoutId.map { inflater.inflate(it, this, false).also { addView(it, params) } }
+            _error = _errorLayoutId.map { inflater.inflate(it, this, false).also { addView(it, params) } }
+            _errorMessage = Option.monad().binding { _error.bind().findViewById<TextView>(_errorMessageId.bind()) }.fix()
+            _retry = Option.monad().binding { _error.bind().findViewById<View>(_errorRetryId.bind()) }.fix()
+            _retry.map { it.setOnClickListener { _retryAction.map { it() } } }
         }
     }
 }
