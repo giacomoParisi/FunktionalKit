b1344e8b70bd56ff3a2f22d190be78964f984ad7
Giacomo Parisi <giacomo@balzo.eu>
2018-09-07 14:35:07 +0200

    Fixed init lambda in viewModelFactory

--- core/src/main/java/com/giacomoparisi/funktionalkit/core/arch/ViewModelFactory.kt
+++ core/src/main/java/com/giacomoparisi/funktionalkit/core/arch/ViewModelFactory.kt
@@ -14,12 +14,12 @@ import javax.inject.Provider
 class ViewModelFactory(val testProvider: (() -> ViewModel)? = null) {
 
     inline fun <reified VM : ViewModel> factory(
-            provider: Provider<VM>, crossinline init: (VM) -> Unit
+            provider: Provider<VM>
     ): ViewModelProvider.Factory {
         return object : ViewModelProvider.Factory {
             override fun <T1 : ViewModel> create(aClass: Class<T1>): T1 {
                 val viewModel = testProvider?.invoke() as VM? ?: provider.get()
-                return viewModel.also(init) as T1
+                return viewModel as T1
             }
         }
     }
@@ -27,12 +27,12 @@ class ViewModelFactory(val testProvider: (() -> ViewModel)? = null) {
     inline operator fun <reified VM : ViewModel> invoke(
             fragment: Fragment, provider: Provider<VM>, crossinline init: (VM) -> Unit = {}
     ): VM {
-        return ViewModelProviders.of(fragment, factory(provider, init)).get(VM::class.java)
+        return ViewModelProviders.of(fragment, factory(provider)).get(VM::class.java).also(init)
     }
 
     inline operator fun <reified VM : ViewModel> invoke(
             activity: FragmentActivity, provider: Provider<VM>, crossinline init: (VM) -> Unit = {}
     ): VM {
-        return ViewModelProviders.of(activity, factory(provider, init)).get(VM::class.java)
+        return ViewModelProviders.of(activity, factory(provider)).get(VM::class.java).also(init)
     }
 }
